4977872b7ae0b4091503ade3f67808c026d8a782
sunnysavita10 <engsunny3365@gmail.com>
2024-06-23 19:50:03 +0530

    code updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name="financebot",
+    version="0.0.1",
+    author="sunny",
+    author_email="sunny.savita@ineuron.ai",
+    packages =find_packages(),
+    install_requires=["langchain","langchain-openai","langchain-astradb","datasets","pypdf","python-dotenv","flask"]
+)
